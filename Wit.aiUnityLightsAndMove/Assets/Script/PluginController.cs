18b1f239b89d004b362a7879f4a6fd01f265d4d7
roxio93 <xxpasquxx@gmail.com>
2015-04-30 10:56:50 +0200

    Unity progect that use voice to control the ambient directional light

---
+++ Wit.aiUnityLightsAndMove/Assets/Script/PluginController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityHttpReq;
+
+public class PluginController : MonoBehaviour {
+
+	string wit_code = "EIZVGRFJR4THCTATYLNAAMOXIUWBPV7N";
+	string filepath = @"C:\Users\Public\Documents\Unity Projects\Wit.aiUnityLightsAndMove\";
+
+    string filename = @"lightsoff.wav";
+
+   
+    // NLP_Processing is the code that processes the response from wit.ai
+    NLP_Processing processor;
+
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+		
+		print ("Attivo Witai");
+        processor = new NLP_Processing(wit_code);
+		
+		string file = filepath+filename;
+        processor.ProcessSpokenFile(file);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
